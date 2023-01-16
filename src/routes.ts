96a60f58f580b3fa9e8b5eb758906e3b81032e0f
Deivid <deividgabrielproc@gmail.com>
2023-01-16 13:27:16 -0300

    update: Added routes for API access

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,30 @@
 import { Router } from 'express'
+import { BookController } from './controller/book-controller'
+import { InMemoryBookRepository } from './repositories/in-memory-book-repository'
 
+const bookController = new BookController(new InMemoryBookRepository())
 export const router = Router()
 
 router.get('/', (req, res) => {
-  return res.json({ hello: 'World' })
+  void bookController.list().then((books) => {
+    res.json(books)
+  })
+})
+
+router.post('/', (req, res) => {
+  void bookController.create(req.body).then((book) => {
+    res.status(201).json(book)
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  void bookController.delete(Number(req.params.id)).then(() => {
+    res.json({ deleted: true })
+  })
+})
+
+router.put('/', (req, res) => {
+  void bookController.update(req.body).then((book) => {
+    res.json(book)
+  })
 })
