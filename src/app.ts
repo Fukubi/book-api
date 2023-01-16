3ab7299e6fb8578f399e30deb7b8e611c2c0bbb5
Deivid <deividgabrielproc@gmail.com>
2023-01-16 13:09:24 -0300

    Starter setup for express

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv'
+import express from 'express'
+import { router } from './routes'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(router)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on http://localhost:${process.env.PORT as string}/`)
+})
